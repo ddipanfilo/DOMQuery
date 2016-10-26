81fbafe80f3611837b077c7e11922422c6422f03
ddipanfilo <ddipanfilo@gmail.com>
2016-10-26 18:34:56 -0400

    Remove unused code/comments

--- js/board.js
+++ js/board.js
@@ -10,31 +10,6 @@ class Board {
     this.score = 0;
   }
 
-  static blankGrid(dim) {
-    const grid = [];
-
-    for (let i = 0; i < dim; i++) {
-      const row = [];
-      for (let j = 0; j < dim; j++) {
-        row.push(".");
-      }
-      grid.push(row);
-    }
-
-    return grid;
-  }
-
-  render() {
-    const blankGrid = Board.blankGrid(this.dim);
-
-    this.snake.segments.forEach( segment => {
-      blankGrid[segment.i][segment.j] = "S";
-    });
-
-    const rowStrs = [];
-    blankGrid.map( row => row.join("") ).join("\n");
-  }
-
   validPosition(coordinate) {
     return (coordinate.i >= 0) && (coordinate.i < this.dim) &&
     (coordinate.j >= 0) && (coordinate.j < this.dim);
